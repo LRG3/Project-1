21a4c82254ccf19b3b5b2b54aba913cd6cd9a4cc
LRG3 <lrg2013@yahoo.com>
2018-11-15 21:35:49 -0700

    trying to pull master

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,6 @@
-var beaches= ["Kapalua Bay Beach, Maui, Hawaii", "Ocracoke Lifeguarded Beach, Outer Banks, North Carolina", "Grayton Beach State Park, Florida Panhandle", 
+var beaches = ["Kapalua Bay Beach, Maui, Hawaii", "Ocracoke Lifeguarded Beach, Outer Banks, North Carolina", "Grayton Beach State Park, Florida Panhandle", 
 "Coopers Beach, Southampton, New York","Coast Guard Beach, Cape Cod, Massachusetts","Lighthouse Beach, Buxton, Outer Banks, North Carolina",
 "Caladesi Island State Park, Dunedin/Clearwater, Florida","Hapuna Beach State Park, Big Island, Hawaii","Coronado Beach, San Diego, California",
-"Beachwalker Park, Kiawah Island, South Carolina"]
+"Beachwalker Park, Kiawah Island, South Carolina"];
+
+var beachrandom = beaches[Math.floor(Math.random() * beaches.length)];
