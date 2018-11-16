06c39cfe2db0b9504a299cbf7da390d94f455b94
LRG3 <lrg2013@yahoo.com>
2018-11-15 20:09:50 -0700

    draft 1

---
+++ assets/javascript/main.js
@@ -0,0 +1,4 @@
+var beaches= ["Kapalua Bay Beach, Maui, Hawaii", "Ocracoke Lifeguarded Beach, Outer Banks, North Carolina", "Grayton Beach State Park, Florida Panhandle", 
+"Coopers Beach, Southampton, New York","Coast Guard Beach, Cape Cod, Massachusetts","Lighthouse Beach, Buxton, Outer Banks, North Carolina",
+"Caladesi Island State Park, Dunedin/Clearwater, Florida","Hapuna Beach State Park, Big Island, Hawaii","Coronado Beach, San Diego, California",
+"Beachwalker Park, Kiawah Island, South Carolina"]
