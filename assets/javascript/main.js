fd038de0a97d114f36f794874d34d1294fe1fe04
LRG3 <lrg2013@yahoo.com>
2018-11-27 21:45:27 -0700

    update 76

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,8 @@
 //Random Beach Array
 var beaches = ["Kapalua Bay Beach, Maui, Hawaii", "Ocracoke Lifeguarded Beach, Outer Banks, North Carolina", "Grayton Beach State Park, Florida Panhandle",
-    "Coopers Beach, Southampton, New York", "Coast Guard Beach, Cape Cod, Massachusetts", "Lighthouse Beach, Buxton, Outer Banks, North Carolina",
-    "Caladesi Island State Park, Dunedin/Clearwater, Florida", "Hapuna Beach State Park, Big Island, Hawaii", "Coronado Beach, San Diego, California",
-    "Beachwalker Park, Kiawah Island, South Carolina"];
+"Coopers Beach, Southampton, New York", "Coast Guard Beach, Cape Cod, Massachusetts", "Lighthouse Beach, Buxton, Outer Banks, North Carolina",
+"Caladesi Island State Park, Dunedin/Clearwater, Florida", "Hapuna Beach State Park, Big Island, Hawaii", "Coronado Beach, San Diego, California",
+"Beachwalker Park, Kiawah Island, South Carolina"];
 
 var hotelData = [
     {
@@ -72,6 +72,58 @@ var hotelData = [
 // Airport Array - order matches beach array (substituted major airports as hotel API did not recognize some smaller airports)
 var destinationCode = ["OGG", "EWN", "ECP", "JFK", "MVY", "EWN", "PIE", "KOA", "SAN", "HHH"]
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyChELzcTpWOe3vESiwGZsUjoCNjeapooV4",
+    authDomain: "beach-out.firebaseapp.com",
+    databaseURL: "https://beach-out.firebaseio.com",
+    projectId: "beach-out",
+    storageBucket: "beach-out.appspot.com",
+    messagingSenderId: "502652986769"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#vacation-button").on("click", function (event) {
+    event.preventDefault();
+
+    var userEmail = $("#email-add").val().trim();
+
+database.ref().push(userEmail);
+})
+
+//these are just helper functions that can manupulate the DOM
+//in this case we just get rid of the vacation Button
+//and add and remove some classes
+function removeGoButton() {
+    $('#vacation-button').hide();
+    $('#email-add').removeClass('email-good')
+    $('#email-add').addClass('email-bad')
+}
+function addGoButton() {
+    $('#vacation-button').show();
+    $('#email-add').removeClass('email-bad')
+    $('#email-add').addClass('email-good')
+}
+
+//this just hides the "Vacation button" as per the above
+$('#vacation-button').hide();
+
+function validateEmail() {
+    var x = $('#email-add').val();
+
+    if (x.indexOf("@") === -1) {
+        removeGoButton();
+        //this is used down below to see if it's valid or not
+        return false;
+    } else {
+        addGoButton();
+        $('#vacation-button').prop("disabled", false);
+        //this is used down below to see if it's valid or not
+        return true;
+    }
+}
 //Generates Random Beach Location
 var beachrandom = beaches[Math.floor(Math.random() * beaches.length)];
 
@@ -105,7 +157,7 @@ function populateHotelData() {
 
 //This function runs the whole application... parameters are used to provide the data from user.
 function displayCityData(beachLocation, depDate, returnDate, userAirportCode) {
-    //Lines 25-68 are for Restaurant API
+    //Restaurant API
     var clientID = "-_-qsu9VN_Mu1cRQ_CEfbA"
     var apikey = "eGyFYoGa3oYrHwELLpuXsE9A1l9W6d6AoJszCKMPa3M9SNgR2kx1md-nelFS1jJdfOb1sCD3knBmuWA7kDTZSoZMehkn0-Avx1VDY6QMhAX45RpIuKyxSBZ53eTsW3Yx"
     var queryURL = "https://developers.zomato.com/api/v2.1/locations?query=" + beachLocation + "&apikey=937d785cc1c1b2ac1098e43a13b9cf22";
@@ -127,16 +179,9 @@ function displayCityData(beachLocation, depDate, returnDate, userAirportCode) {
             var topRestaurantURL = response.best_rated_restaurant[0].restaurant.url;
             var topRestaurantAddress = response.best_rated_restaurant[0].restaurant.location.address;
             var avgCostForTwo = response.best_rated_restaurant[0].restaurant.average_cost_for_two
-            $('.ResName').html(`<a href="${topRestaurantURL}" target="blank">${topRestaurant}</a>`)
-            $('.ResforTwo').append("$" + avgCostForTwo);
-            $('.ResAddress').append(topRestaurantAddress);
-
             var restaurantID = response.best_rated_restaurant[0].restaurant.R.res_id;
-            console.log(restaurantID)
+            
             var restaurntDetailQueryURL = "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + restaurantID + "&apikey=937d785cc1c1b2ac1098e43a13b9cf22"
-
-
-
             $.ajax({
                 url: restaurntDetailQueryURL,
                 method: "GET"
@@ -144,57 +189,71 @@ function displayCityData(beachLocation, depDate, returnDate, userAirportCode) {
                 var restaurantLocation = response.location.address
                 var avgCost = response.average_cost_for_two;
                 var cuisines = response.cuisines;
-                console.log(restaurantLocation, avgCost, cuisines);
-                $('.Cuisines').append(cuisines);
-
+                //Flight API
+                $.ajax({
+                    url: "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=HEbMkphMv3ReciH7JfBcCzGQLfGG2UCk&origin=" + userAirportCode + "&destination=" + selectedDestinationCode + "&departure_date=" + depDate + "&return_date=" + returnDate + "&number_of_results=1",
+                    method: "GET"
+                }).then(function (response) {
+                    var flightPrice = response.results[0].fare.total_price;
+                    var departsAt = moment(response.results[0].itineraries[0].outbound.flights[0].departs_at).format("MM/DD/YYYY hh:mm a ");
+                    var arrivesAt = moment(response.results[0].itineraries[0].outbound.flights[(response.results[0].itineraries[0].outbound.flights.length - 1)].arrives_at).format("MM/DD/YYYY hh:mm a");
+                    var duration = response.results[0].itineraries[0].outbound.duration;
+                    var airlineCode = response.results[0].itineraries[0].outbound.flights[0].operating_airline;
+                    var retDepartsAt = moment(response.results[0].itineraries[0].inbound.flights[0].departs_at).format("MM/DD/YYYY hh:mm a ");
+                    var retArrivesAt = moment(response.results[0].itineraries[0].inbound.flights[(response.results[0].itineraries[0].inbound.flights.length - 1)].arrives_at).format("MM/DD/YYYY hh:mm a");
+                    var retDuration = response.results[0].itineraries[0].inbound.duration;
+                    var retAirlineCode = response.results[0].itineraries[0].inbound.flights[0].operating_airline;
+                    console.log(retDepartsAt, retAirlineCode, retArrivesAt, retDuration)
+                    $('.Cuisines').append(cuisines);
+                    $('.ResName').html(`<a href="${topRestaurantURL}" target="blank">${topRestaurant}</a>`)
+                    $('.ResforTwo').append("$" + avgCostForTwo);
+                    $('.ResAddress').append(topRestaurantAddress);                    
+                    $(".ArrivalAirport").append(`<a href="https://en.wikipedia.org/wiki/List_of_airports_by_IATA_code:_A" target="blank">${selectedDestinationCode}</a>`)
+                    $(".Airline").append(`<a href="https://www.airfarewatchdog.com/airline-codes/" target="blank">${airlineCode}</a>`)
+                    $(".FlightPrice").append("$" + flightPrice);
+                    $(".LeaveDate").append(departsAt);
+                    $(".FlightArrival").append(arrivesAt);
+                    $(".Duration").append(duration);
+                    $(".retFlightPrice").append("Above");
+                    $(".retArrivalAirport").append(`<a href="https://en.wikipedia.org/wiki/List_of_airports_by_IATA_code:_A" target="blank">${userAirportCode}</a>`)
+                    $(".retAirline").append(`<a href="https://www.airfarewatchdog.com/airline-codes/" target="blank">${retAirlineCode}</a>`)
+                    $(".retLeaveDate").append(retDepartsAt);
+                    $(".retFlightArrival").append(retArrivesAt);
+                    $(".retDuration").append(retDuration);
+                    
+                    populateHotelData();
+                }).catch(function (error) {
+                    $('td').empty();
+                    alert(error.responseJSON.message);
+                    $('td').empty();
+                })
             })
         })
-    })
+        
+        //Hotel API --- NOT WORKING(AMADEUS SUPPORT WORKING ON ISSUE)  REPLACEMENT BAND AID ABOVE
+        // var hotelQueryURL = "https://api.sandbox.amadeus.com/v1.2/hotels/search-airport/?apikey=KWxHu3JXbSfuZ0ZfU5AmxbNfkmVaKQTX&location=" + selectedDestinationCode + "&check_in=" + depDate + "&check_out=" + returnDate;
+        // var hotelQueryURL = "https://api.sandbox.amadeus.com/v1.2/hotels/search-airport/?apikey=KWxHu3JXbSfuZ0ZfU5AmxbNfkmVaKQTX&location=" + selectedDestinationCode + "&check_in=2019-01-02&check_out=2019-01-09"
+        // $.ajax({
+        //     url: hotelQueryURL,
+        //     method: "GET"
+        // }).then(function (response) {
+        //     console.log(JSON.stringify(response));
+        // var hotelName = response.results[0].property_name;
+        // var hotelPrice = response.results[0].total_price.amount;
+        // var hotelStreet = response.results[0].address.line1;
+        // var hotelCity = response.results[0].address.city;
+        // var hotelState = response.results[0].address.region;
+        // var hotelPostal = response.results[0].address.postal_code;
+        // var fullAddress = hotelStreet + "" + hotelCity + "" + hotelState + "" + hotelPostal;
+        // var roomType = response.results[0].rooms[0].room_type_info.room_type;
+
+        // $(".HoName").append(hotelName);
+        // $(".HoPrice").append("$" + hotelPrice);
+        // $(".HoAddress").append(fullAddress);
+        // $(".HoRoomType").append(roomType);
+        // })
 
 
-
-
-
-    //Hotel API --- NOT WORKING(AMADEUS SUPPORT WORKING ON ISSUE)  REPLACEMENT BAND AID ABOVE
-    // var hotelQueryURL = "https://api.sandbox.amadeus.com/v1.2/hotels/search-airport/?apikey=KWxHu3JXbSfuZ0ZfU5AmxbNfkmVaKQTX&location=" + selectedDestinationCode + "&check_in=" + depDate + "&check_out=" + returnDate;
-    // var hotelQueryURL = "https://api.sandbox.amadeus.com/v1.2/hotels/search-airport/?apikey=KWxHu3JXbSfuZ0ZfU5AmxbNfkmVaKQTX&location=" + selectedDestinationCode + "&check_in=2019-01-02&check_out=2019-01-09"
-    // $.ajax({
-    //     url: hotelQueryURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(JSON.stringify(response));
-    // var hotelName = response.results[0].property_name;
-    // var hotelPrice = response.results[0].total_price.amount;
-    // var hotelStreet = response.results[0].address.line1;
-    // var hotelCity = response.results[0].address.city;
-    // var hotelState = response.results[0].address.region;
-    // var hotelPostal = response.results[0].address.postal_code;
-    // var fullAddress = hotelStreet + "" + hotelCity + "" + hotelState + "" + hotelPostal;
-    // var roomType = response.results[0].rooms[0].room_type_info.room_type;
-
-    // $(".HoName").append(hotelName);
-    // $(".HoPrice").append("$" + hotelPrice);
-    // $(".HoAddress").append(fullAddress);
-    // $(".HoRoomType").append(roomType);
-    // })
-
-
-    $(".ArrivalAirport").append(`<a href="https://en.wikipedia.org/wiki/List_of_airports_by_IATA_code:_A" target="blank">${selectedDestinationCode}</a>`)
-    //Lines 95-112 are Flight API
-    $.ajax({
-        url: "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=HEbMkphMv3ReciH7JfBcCzGQLfGG2UCk&origin=" + userAirportCode + "&destination=" + selectedDestinationCode + "&departure_date=" + depDate + "&return_date=" + returnDate + "&number_of_results=1",
-        method: "GET"
-    }).then(function (response) {
-        var flightPrice = response.results[0].fare.total_price;
-        var departsAt = moment(response.results[0].itineraries[0].outbound.flights[0].departs_at).format("MM/DD/YYYY hh:mm a ");
-        var arrivesAt = moment(response.results[0].itineraries[0].outbound.flights[(response.results[0].itineraries[0].outbound.flights.length - 1)].arrives_at).format("MM/DD/YYYY hh:mm a");
-        var duration = response.results[0].itineraries[0].outbound.duration;
-        var airlineCode = response.results[0].itineraries[0].outbound.flights[0].operating_airline;
-        $(".Airline").append(`<a href="https://www.airfarewatchdog.com/airline-codes/" target="blank">${airlineCode}</a>`)
-        $(".FlightPrice").append("$" + flightPrice);
-        $(".LeaveDate").append(departsAt);
-        $(".FlightArrival").append(arrivesAt);
-        $(".Duration").append(duration);
     })
 }
 
@@ -203,11 +262,31 @@ $('#vacation-button').on("click", function () {
     $('td').empty();
     depDate = $("#departure-date").val();
     returnDate = $("#returnDate").val();
-    userAirportCode = $("#user-airport").val().trim();
+    userAirportCode = $("#user-airport").val().trim().toUpperCase();
     displayCityData(beachrandom, depDate, returnDate, userAirportCode);
-    populateHotelData();
     beachrandom = beaches[Math.floor(Math.random() * beaches.length)];
     selectedHotel = hotelData[0][beachrandom];
     generateAirportCode();
-    
+
 })
+
+//This does validation whever that input form changes
+$("#email-add").keyup(function () {
+    validateEmail();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
